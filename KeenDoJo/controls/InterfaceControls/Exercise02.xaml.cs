09b1c12a4ad54926c19d129ffef37b8c92287a1d
Keenan Stewart <kstewart@boumatic.com>
2022-10-12 10:28:51 -0300

    Added better coordinates to compare points of interest on my funnel or other controls, such as the Navigation controls I am creating. - Need to make a library of navigational controlsAdded logic to draw multiple points at once that are listed in a textbox

--- KeenDoJo/controls/InterfaceControls/Exercise02.xaml.cs
+++ KeenDoJo/controls/InterfaceControls/Exercise02.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KeenDoJo.controls.InterfaceControls
@@ -23,6 +15,8 @@ namespace KeenDoJo.controls.InterfaceControls
 		// Create global colors for now?
 		public Brush WhiteBrush { get; set; } = Brushes.White;
 		public Brush BlackBrush { get; set; } = Brushes.Black;
+		public Brush GrayBrush { get; set; }
+			= (Brush)new BrushConverter().ConvertFrom("#87888a");
 
 		// #597BB1
 		public Brush BlueBrush { get; set; }
@@ -40,6 +34,48 @@ namespace KeenDoJo.controls.InterfaceControls
 		public Exercise02()
 		{
 			InitializeComponent();
+
+			MakeEllipses();
+		}
+
+		private void MakeEllipses()
+		{
+			double EllipseWidth = 100;
+			double EllipseHeight = 100;
+
+			double outerEllipseCenter = 0.95;
+			double innerEllipseCenter = 0.05;
+			double ellipsePosition = ((EllipseWidth * outerEllipseCenter)
+				- (EllipseWidth * innerEllipseCenter)) / 2;
+
+			Ellipse outerEllipse = new Ellipse();
+			outerEllipse.Width = EllipseWidth * outerEllipseCenter;
+			outerEllipse.Height = EllipseHeight * outerEllipseCenter;
+			outerEllipse.Fill = GrayBrush;
+			outerEllipse.Stroke = GrayBrush;
+			outerEllipse.StrokeThickness = 1;
+			outerEllipse.ToolTip = "outerEllipse: Exercise 02";
+			outerEllipse.Name = "outerEllipse";
+
+			Ellipse innerEllipse = new Ellipse();
+			innerEllipse.Width = EllipseWidth * 0.05;
+			innerEllipse.Height = EllipseHeight * 0.05;
+			//innerEllipse.BringIntoView();
+			innerEllipse.Fill = Brushes.White;
+			innerEllipse.Stroke = Brushes.White;
+			innerEllipse.StrokeThickness = 1;
+			innerEllipse.ToolTip = "innerEllipse: Exercise 02";
+			innerEllipse.Name = "innerEllipse";
+
+			Canvas.SetLeft(innerEllipse, ellipsePosition);
+			Canvas.SetTop(innerEllipse, ellipsePosition);
+
+			Canvas.SetZIndex(innerEllipse, 99);
+			Canvas.SetZIndex(outerEllipse, 98);
+
+			FunnelDataPathCanvas.Children.Add(outerEllipse);
+			FunnelDataPathCanvas.Children.Add(innerEllipse);
+			FunnelDataPathCanvas.Background = Brushes.Tan;
 		}
 
 		#region Work
@@ -49,6 +85,93 @@ namespace KeenDoJo.controls.InterfaceControls
 			// Ensure all textboxes have a value, they have default values
 			if (CheckNulls()) return;
 
+			// Create points or a single Ellipse
+			if (txtPoints.Text != "")
+			{
+				if ((bool)!RetainEllipseCheckBox.IsChecked)
+					ClearEllipseButton_Click(sender, e);
+				CreateEllipses(txtPoints.Text);
+			}
+
+			else
+			{
+				CreateEllipseSingle();
+			}
+		}
+
+		private void CreateEllipses(string text)
+		{
+			char[] chars = new[] { '\r', '\n' };
+
+			string[] strings = text.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+			int k = 1;
+
+			foreach (string strLine in strings)
+			{
+				Console.WriteLine(strLine);
+				CreateEllise(strLine, k);
+				k++;
+			}
+		}
+
+		// TODO: Put all CreateEllipse in common function, once we think about it
+		private void CreateEllise(string strLine, int k)
+		{
+			Ellipse ellipse = new Ellipse
+			{
+				Width = Convert.ToDouble(WidthTextBox.Text),
+				Height = Convert.ToDouble(HeightTextBox.Text),
+				Stroke = Brushes.Red,
+				StrokeThickness = 1,
+				Fill = Brushes.Blue,
+				Visibility = Visibility.Visible
+			};
+
+			ellipse.Fill = (SolidColorBrush)new BrushConverter().ConvertFromString(FillTextBox.Text);
+			ellipse.Stroke = (SolidColorBrush)new BrushConverter().ConvertFromString(StrokeTextBox.Text);
+
+			string strToolTip = "(" + strLine + ") # " + k;
+			ellipse.ToolTip = strToolTip;
+
+			// TODO: Double check to ensure name not already in use
+			ellipse.Name = "Ellipse" + FunnelDataPathCanvas.Children.Count;
+			ellipse.Uid = "Ellipse" + FunnelDataPathCanvas.Children.Count;
+
+			ellipse.MouseUp += Ellipse_MouseUp;
+
+			//char[] chars = { ' ', '\t' };
+			char[] chars = { ',' };
+			string[] coordinates = strLine.Split(chars);
+						
+			double[] doubles = new double[coordinates.Length];
+
+			if ((doubles[0] == 0) && (doubles[1] == 0)) return;
+
+			try
+			{
+				for (int i = 0; i < coordinates.Length; i++)
+				{
+					doubles[i] = Convert.ToDouble(coordinates[i]);
+				}
+			}
+
+			catch (Exception ex)
+			{
+				// TODO: add more stringent error handling.
+				// TODO: - if two points on a single line, process anyway
+				string strMessage = "Any errors with coordinates, check input to ensure only " +
+					"one point per line.  Also check Console Messages: " + ex.Message;
+				MessageBox.Show(strMessage);
+			}
+
+			Canvas.SetLeft(ellipse, doubles[0] - (Convert.ToDouble(WidthTextBox.Text) / 2));
+			Canvas.SetTop(ellipse, doubles[1] - (Convert.ToDouble(HeightTextBox.Text)) / 2);
+			Canvas.SetZIndex(ellipse, 99);
+			FunnelDataPathCanvas.Children.Add(ellipse);
+		}
+
+		private void CreateEllipseSingle()
+		{
 			Ellipse ellipse = new Ellipse
 			{
 				Width = Convert.ToDouble(WidthTextBox.Text),
@@ -73,7 +196,6 @@ namespace KeenDoJo.controls.InterfaceControls
 			Canvas.SetTop(ellipse, (Convert.ToDouble(TopTextBox.Text)) - (Convert.ToDouble(HeightTextBox.Text)) / 2);
 			Canvas.SetZIndex(ellipse, 99);
 			FunnelDataPathCanvas.Children.Add(ellipse);
-			//EllipsePracticeAreaGrid.Children.Add(ellipse);
 		}
 
 		private void Ellipse_MouseUp(object sender, MouseButtonEventArgs e)
@@ -149,7 +271,14 @@ namespace KeenDoJo.controls.InterfaceControls
 			{
 				UIElement child = FunnelDataPathCanvas.Children[k];
 				if (child is Ellipse)
-					FunnelDataPathCanvas.Children.Remove(child);
+				{
+					if ((child as Grid) != null)
+					{
+						if (((child as Grid).Name != "innerEllipse") ||
+							((child as Grid).Name != "outerEllipse"))
+							FunnelDataPathCanvas.Children.Remove(child);
+					}
+				}
 			}
 		}
 
