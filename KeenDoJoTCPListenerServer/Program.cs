6b3bab46013c615a1f4b30c38314d1727a75a71d
Keenan Stewart <kstewart@boumatic.com>
2023-01-27 14:27:41 -0400

    Testing TCP clients/servers

---
+++ KeenDoJoTCPListenerServer/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// TCP listener Server
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//int PLCPort = 60111;
+//int PLCPort = 50823;
+int PLCPort = 50827;
+
+Console.WriteLine("KeenDoJoTcpListenerServer startup");
+
+var ipEndPoint = new IPEndPoint(IPAddress.Loopback, PLCPort);
+//var ipEndPoint = new IPEndPoint(IPAddress.Any, PLCPort);
+TcpListener listener = new TcpListener(ipEndPoint);
+
+try
+{
+	listener.Start();
+
+	while (true)
+	{
+		using TcpClient handler = await listener.AcceptTcpClientAsync();
+		await using NetworkStream stream = handler.GetStream();
+
+		var message = "Melanie is my girlfriend!";
+		var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+		await stream.WriteAsync(dateTimeBytes);
+
+		Console.Write(".");
+	}
+}
+
+finally
+{
+	listener.Stop();
+}
---
+++ SomeName/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// TCP Client
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("KeenDoJoTcpClient Startup");
+
+//int port = 64659;
+//int port = 60111;
+int port = 50823;
+
+var ipEndPoint = new IPEndPoint(IPAddress.Loopback, port);
+
+TcpClient client = new();
+
+try
+{
+	while (true)
+	{
+		client = new();
+		await client.ConnectAsync(ipEndPoint);
+		await using NetworkStream stream = client.GetStream();
+
+		var buffer = new byte[1024];
+		int received = await stream.ReadAsync(buffer);
+
+		var message = Encoding.UTF8.GetString(buffer, 0, received);
+
+		Console.WriteLine("message is: " + message);
+	}
+}
+
+finally
+{
+	client.Close();
+}
