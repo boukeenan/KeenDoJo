86833835f10cc692b79ce94e9a59d3b3d1d53c75
Keenan Stewart <kstewart@boumatic.com>
2023-08-03 12:42:57 -0300

    wip, bitwise

---
+++ KeenBitWise/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace KeenBitWise
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmd32BitValue = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.lbl32BitValue = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txt32BitValue = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.lblValue1 = new System.Windows.Forms.Label();
+			this.lblValue2 = new System.Windows.Forms.Label();
+			this.cmdClose = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// cmd32BitValue
+			// 
+			this.cmd32BitValue.Location = new System.Drawing.Point(357, 97);
+			this.cmd32BitValue.Name = "cmd32BitValue";
+			this.cmd32BitValue.Size = new System.Drawing.Size(75, 23);
+			this.cmd32BitValue.TabIndex = 0;
+			this.cmd32BitValue.Text = "Convert";
+			this.cmd32BitValue.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(357, 144);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(357, 190);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(357, 249);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// lbl32BitValue
+			// 
+			this.lbl32BitValue.AutoSize = true;
+			this.lbl32BitValue.Location = new System.Drawing.Point(45, 101);
+			this.lbl32BitValue.Name = "lbl32BitValue";
+			this.lbl32BitValue.Size = new System.Drawing.Size(67, 15);
+			this.lbl32BitValue.TabIndex = 4;
+			this.lbl32BitValue.Text = "32 bit value";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(45, 148);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 15);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "label2";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(45, 198);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(38, 15);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(45, 257);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(38, 15);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "label4";
+			// 
+			// txt32BitValue
+			// 
+			this.txt32BitValue.Location = new System.Drawing.Point(153, 93);
+			this.txt32BitValue.Name = "txt32BitValue";
+			this.txt32BitValue.Size = new System.Drawing.Size(100, 23);
+			this.txt32BitValue.TabIndex = 8;
+			this.txt32BitValue.Text = "100003";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(153, 140);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 23);
+			this.textBox2.TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(153, 190);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 23);
+			this.textBox3.TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(153, 249);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 23);
+			this.textBox4.TabIndex = 11;
+			// 
+			// lblValue1
+			// 
+			this.lblValue1.AutoSize = true;
+			this.lblValue1.Location = new System.Drawing.Point(507, 101);
+			this.lblValue1.Name = "lblValue1";
+			this.lblValue1.Size = new System.Drawing.Size(41, 15);
+			this.lblValue1.TabIndex = 12;
+			this.lblValue1.Text = "Value1";
+			// 
+			// lblValue2
+			// 
+			this.lblValue2.AutoSize = true;
+			this.lblValue2.Location = new System.Drawing.Point(636, 101);
+			this.lblValue2.Name = "lblValue2";
+			this.lblValue2.Size = new System.Drawing.Size(41, 15);
+			this.lblValue2.TabIndex = 13;
+			this.lblValue2.Text = "Value2";
+			// 
+			// cmdClose
+			// 
+			this.cmdClose.Location = new System.Drawing.Point(700, 399);
+			this.cmdClose.Name = "cmdClose";
+			this.cmdClose.Size = new System.Drawing.Size(75, 23);
+			this.cmdClose.TabIndex = 14;
+			this.cmdClose.Text = "Close";
+			this.cmdClose.UseVisualStyleBackColor = true;
+			this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.cmdClose);
+			this.Controls.Add(this.lblValue2);
+			this.Controls.Add(this.lblValue1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.txt32BitValue);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lbl32BitValue);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.cmd32BitValue);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Button cmd32BitValue;
+		private Button button2;
+		private Button button3;
+		private Button button4;
+		private Label lbl32BitValue;
+		private Label label2;
+		private Label label3;
+		private Label label4;
+		private TextBox txt32BitValue;
+		private TextBox textBox2;
+		private TextBox textBox3;
+		private TextBox textBox4;
+		private Label lblValue1;
+		private Label lblValue2;
+		private Button cmdClose;
+	}
+}
---
+++ KeenBitWise/Form1.cs
@@ -0,0 +1,15 @@
+namespace KeenBitWise
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void cmdClose_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
