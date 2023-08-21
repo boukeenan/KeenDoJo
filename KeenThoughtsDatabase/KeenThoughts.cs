c6ad6ae8ce95d842475c7aafab37cddbb29ea1bd
Keenan Stewart <kstewart@boumatic.com>
2023-08-21 14:54:21 -0300

    keen-thoughts starter

---
+++ KeenThoughtsDatabase/KeenThoughts.cs
@@ -0,0 +1,24 @@
+﻿namespace KeenThoughtsDatabase
+{
+	public class KeenThoughts
+	{
+		public string KeenThoughtsString { get; set; } = "I figured out the r-word.";
+		public string message { get; set; }
+
+
+		public KeenThoughts()
+		{
+			message += "programming meditation";
+			message += "type out the crazy thoughts in head to sort them out.";
+			message += "";
+			message += "";
+			message += "";
+			message += "";
+			message += "";
+			message += "";
+			message += "";
+			message += "";
+			message += "";
+		}
+	}
+}
