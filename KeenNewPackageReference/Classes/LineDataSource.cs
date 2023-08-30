db4123f7e7d7f2a92aae08396a72b801eb59261f
Keenan Stewart <kstewart@boumatic.com>
2023-08-30 17:18:21 -0300

    Working with Charts

---
+++ KeenNewPackageReference/Classes/LineDataSource.cs
@@ -0,0 +1,8 @@
+﻿namespace KeenNewPackageReference.Classes
+{
+	public class LineDataSource
+	{
+		public string LineName { get; set; }
+		public int LineValue { get; set; }
+	}
+}
---
+++ KeenNewPackageReference/DataSource/WindowDataSource.cs
@@ -0,0 +1,48 @@
+﻿using KeenNewPackageReference.Classes;
+using System.Collections.ObjectModel;
+
+namespace KeenNewPackageReference.DataSource
+{
+	public class WindowDataSource
+	{
+		public ObservableCollection<LineDataSource> LineInformation { get; set; } = GetLineInformation();
+
+		private static ObservableCollection<LineDataSource> GetLineInformation()
+		{
+			return new ObservableCollection<LineDataSource>
+			{
+				new LineDataSource
+				{
+					LineName = "LineOne",
+					LineValue = 6900
+				},
+				new LineDataSource
+				{
+					LineName = "LineTwo",
+					LineValue = 3800
+				},
+				new LineDataSource
+				{
+					LineName = "LineThree",
+					LineValue = 1000
+				},
+				new LineDataSource
+				{
+					LineName = "LineFour",
+					LineValue = 100
+				},
+				new LineDataSource
+				{
+					LineName = "LineFive",
+					LineValue = 800
+				},
+				new LineDataSource
+				{
+					LineName = "LineSix",
+					LineValue = 8800
+				}
+
+			};
+		}
+	}
+}
